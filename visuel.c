a3341fd43a55a6c6f816a674e0614ce604ee118c
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-03-26 21:11:27 +0100

    VISUELS: Fix SQL query 2/2

--- visuel.c
+++ visuel.c
@@ -50,7 +50,7 @@
     gchar *acronyme = Normaliser_chaine ( Json_get_string ( element, "acronyme" ) );
     gboolean cligno = Json_get_bool   ( element, "cligno" );
     DB_Write ( domain_uuid, "UPDATE mnemos_VISUEL SET mode='%s', libelle='%s', color='%s', cligno='%d' "
-                            "WHERE tech_id='%s', acronyme='%s'", mode, libelle, color, cligno, tech_id, acronyme );
+                            "WHERE tech_id='%s' AND acronyme='%s'", mode, libelle, color, cligno, tech_id, acronyme );
     g_free(mode);
     g_free(libelle);
     g_free(color);
