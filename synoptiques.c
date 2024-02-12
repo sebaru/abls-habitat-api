209f6abb5d5ef153480acab90cc61401fd9c733b
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2024-02-12 21:31:49 +0100

    SYNOPTIQUE: Fix Order by clause for heavy syn

--- synoptiques.c
+++ synoptiques.c
@@ -423,9 +423,9 @@
                                 "INNER JOIN dls AS dls_owner ON dls_owner.tech_id=v.tech_id "/* Recup du DLS source du visuel */
                                 "INNER JOIN master.icons AS i USING(forme) "                            /* Lien avec la forme */
                                 "WHERE s.syn_id='%d' AND s.access_level<=%d "
-                                "ORDER BY dls.name, %s",
+                                "ORDER BY %s",
                                 syn_id, user_access_level,
-                                (Json_get_bool(RootNode,"mode_affichage") ? "layer" : "place") );
+                                (Json_get_bool(RootNode,"mode_affichage") ? "layer" : "dls.name, place") );
 
 /*------------------------------------------------- Envoi l'état de tous les visuels du synoptique ---------------------------*/
     Json_node_foreach_array_element ( RootNode, "visuels", VISUEL_Add_etat_to_json, domain );
