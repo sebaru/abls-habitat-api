89bc4fbfb0c337c14cd11d08ab76f9742d483f60
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2024-05-31 21:51:56 +0200

    DLS_TRAD: Add TEMPO in input for New_action_visuel

--- TraductionDLS/Interp.c
+++ TraductionDLS/Interp.c
@@ -1091,7 +1091,14 @@
      { taille = 768;
        action->alors = New_chaine( taille );
        g_snprintf( action->alors, taille,
-                   "  Dls_data_set_VISUEL( vars, _%s_%s, \"%s\", \"%s\", Dls_data_get_WATCHDOG_time (_%s_%s)/10.0, %d, \"%s\", %d );\n",
+                   "  Dls_data_set_VISUEL( vars, _%s_%s, \"%s\", \"%s\", Dls_data_get_WATCHDOG_time (_%s_%s), %d, \"%s\", %d );\n",
+                   alias->tech_id, alias->acronyme, mode, couleur, input->tech_id, input->acronyme, cligno, libelle, disable );
+     }
+    else if (input->classe == T_TEMPO)
+     { taille = 768;
+       action->alors = New_chaine( taille );
+       g_snprintf( action->alors, taille,
+                   "  Dls_data_set_VISUEL( vars, _%s_%s, \"%s\", \"%s\", Dls_data_get_TEMPO_time (_%s_%s), %d, \"%s\", %d );\n",
                    alias->tech_id, alias->acronyme, mode, couleur, input->tech_id, input->acronyme, cligno, libelle, disable );
      }
     else Emettre_erreur_new ( scan_instance, "'%s:%s' is not allowed in 'input'", input->tech_id, input->acronyme );
