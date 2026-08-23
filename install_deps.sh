4082d94ea9b83b2d3798ceac568eb669d072f686
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-08-23 07:53:31 +0200

    REMOVE: Remove ABLS repository configuration from install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -12,9 +12,6 @@ groupadd abls
 
 if [ "$SOCLE" = "fedora" ]
  then
-  echo "Configuring ABLS-PKGS repository"
-  curl -fsSL https://pkgs.abls-habitat.fr/abls-rpms.repo -o /etc/yum.repos.d/abls-rpms.repo
-
   echo "Installing RPM-based dependencies"
   dnf install -y abls-libs-devel git libtool cmake gcc gcc-c++ bison flex
   dnf install -y glib2-devel openssl libsoup3-devel json-glib-devel libjwt-devel
@@ -24,10 +21,6 @@ fi
 
 if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ] || [ "$SOCLE" = "ubuntu" ]
  then
-  echo "Configuring ABLS APT repository"
-  curl -fsSL https://pkgs.abls-habitat.fr/rpms/keys/RPM-GPG-KEY-ABLS | gpg --dearmor -o /usr/share/keyrings/abls-archive-keyring.gpg
-  curl -fsSL https://pkgs.abls-habitat.fr/abls-deb.sources -o /etc/apt/sources.list.d/abls-pkgs.sources
-
   echo "Installing debian/ubuntu dependencies"
 
   apt update -y
